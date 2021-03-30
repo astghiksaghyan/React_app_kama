2e14232407445772d09b4efca5334f0e179c1f16
astghik_s <astghik.saghyan.dc@gmail.com>
2021-03-30 11:04:21 +0400

    Move data to index.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,34 +4,7 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Dialogs from './components/Dialogs/Dialogs';
 import Profiles from './components/Profiles/Profiles';
-const App = () => {
-  const postsData = [
-    {id: 1, message: 'Hy, how are you?', likesCount: 1},
-    {id: 2, message: 'It is my first post', likesCount: 5}
-  ];
-
-  const dialogsData = [
-    {
-        id: 1,
-        name: 'Astghik',
-        message: 'Hi'
-    },
-    {
-        id: 2,
-        name: 'Davit',
-        message: 'How are you?'
-    },
-    {
-        id: 3,
-        name: 'Yerem',
-        message: 'Yo'
-    },
-    {
-        id: 4,
-        name: 'Poghos',
-        message: 'Bo'
-    }
-  ];
+const App = ({postsData, dialogsData}) => {
   return (
     <div className="app-wrapper">
       <BrowserRouter>
--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,37 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+const postsData = [
+  {id: 1, message: 'Hy, how are you?', likesCount: 1},
+  {id: 2, message: 'It is my first post', likesCount: 5}
+];
+
+const dialogsData = [
+  {
+      id: 1,
+      name: 'Astghik',
+      message: 'Hi'
+  },
+  {
+      id: 2,
+      name: 'Davit',
+      message: 'How are you?'
+  },
+  {
+      id: 3,
+      name: 'Yerem',
+      message: 'Yo'
+  },
+  {
+      id: 4,
+      name: 'Poghos',
+      message: 'Bo'
+  }
+];
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App postsData={postsData} dialogsData={dialogsData}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
